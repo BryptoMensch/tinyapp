32cc50ee3a9fcae0964dbfce746bb531018719ca
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-01-24 16:28:38 -0500

    added random string generator, post functionality, body parser etc.

--- express_server.js
+++ express_server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
 
 const urlDatabase = {
 	b2xVn2: "http://www.lighthouselabs.ca",
 	"9sm5xK": "http://www.google.com",
 };
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 	res.send("Hello!");
@@ -36,6 +38,11 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/hello", (req, res) => {
 	res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+	console.log(req.body); // Log the POST request body to the console
+	res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
