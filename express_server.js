f41ccb41605239439af956686cd9bcb40a17a59e
BryptoMensch <90989763+BryptoMensch@users.noreply.github.com>
2022-01-26 15:18:17 -0500

    finished login and log out, cookies module

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 const urlDatabase = {
 	b2xVn2: "http://www.lighthouselabs.ca",
@@ -13,20 +15,28 @@ function generateRandomString() {
 	return randomNum;
 }
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/urls/new", (req, res) => {
-	res.render("urls_new");
+	const templateVars = {
+		username: req.cookies["username"],
+	};
+	res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-	const templateVars = { urls: urlDatabase };
+	const templateVars = {
+		username: req.cookies["username"],
+		urls: urlDatabase,
+	};
 	res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
 	const templateVars = {
+		username: req.cookies["username"],
 		shortURL: req.params.shortURL,
 		longURL: urlDatabase[req.params.shortURL],
 	};
@@ -58,6 +68,17 @@ app.get("/u/:shortURL", (req, res) => {
 	const longURL = urlDatabase[req.params.shortURL];
 	if (longURL === null) return res.sendStatus(404);
 	res.redirect(longURL);
+});
+
+app.post("/login", (req, res) => {
+	res.cookie("username", req.body.username);
+	console.log(req.body);
+	res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+	res.clearCookie('username');
+	res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
